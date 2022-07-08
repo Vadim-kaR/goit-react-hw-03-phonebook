30cabc5380e499a692af28cccbb3bf2b4a73d8e7
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-07-08 17:04:45 +0300

    add local Storage

--- src/App.js
+++ src/App.js
@@ -9,13 +9,28 @@ import { Box } from 'components/Box/Box';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    const parsedContacs = JSON.parse(localContacts);
+    if (parsedContacs) {
+      this.setState({ contacts: parsedContacs });
+    }
+  }
+
+  componentDidUpdate(_, prevUpdate) {
+    const { contacts } = this.state;
+    if (prevUpdate.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   getFiltredContact = () => {
     const { contacts, filter } = this.state;
